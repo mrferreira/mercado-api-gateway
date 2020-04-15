21af410b9f5d15f2354439a7111675cc88f19319
Misael Ferreira <misael.ferreira@gmail.com>
2020-04-15 00:35:32 -0300

    initial commit

---
+++ src/test/java/com/fersoft/mercado/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fersoft.mercado.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
